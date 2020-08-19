83f230d588e2db3f06eed8b562f0cdf319747103
sarahsindet <sarahsindet@gmail.com>
2020-08-19 18:24:04 +0300

    made some changes

--- awward/urls.py
+++ awward/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^create/profile$',views.create_profile, name='create-profile'),
