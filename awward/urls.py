4d1b8922e1e3ef37574579a831c1f95a3d5961a8
sarahsindet <sarahsindet@gmail.com>
2020-08-19 18:25:53 +0300

    made some imports

--- awward/urls.py
+++ awward/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
